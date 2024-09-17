f7f503d56bca46c02dfbb3cdca8ed44e8b5324b0
山本　竜三 <ryu@yamakun.net>
2024-09-17 13:23:21 +0900

    シグナル利用中止

--- processing/scripts/startup.py
+++ processing/scripts/startup.py
@@ -10,27 +10,6 @@ from PyQt5.QtWidgets import QMessageBox
 from qgis.core import QgsExpressionContextUtils
 import os
 
-def on_project_read():
-    # プロジェクトファイルの読み込み開始
-    pass
-
-def on_render_complete():
-    # マップキャンバスの描画完了
-    # プロジェクトが本当に読み込まれていない場合があるため、時間稼ぎが必要であり以下のメッセージ表示は削除しないこと
-    # QGISの変数はすべて小文字であることに注意
-    UserRole_value = QgsExpressionContextUtils.globalScope().variable('userrole')
-    QMessageBox.information(None, "startup.py", "プロジェクトの読み込みが完了しました。\n初期設定を開始します。")
-    print (f"変数　UserRole：{UserRole_value}")
-    if UserRole_value == 'Viewer':
-        set_all_layers_readonly()
-    #QMessageBox.information(None, "startup.py", "プロジェクトの読み込みと初期化が完全に終了しました。")
-    print("プロジェクトの読み込みと初期化が完全に終了しました")
-    pass
-
-def on_all_tasks_finished():
-    # すべてのバックグラウンドタスクの完了
-    pass
-
 def set_all_layers_readonly():
     project = QgsProject.instance()
     layers = project.mapLayers().values()
@@ -71,9 +50,15 @@ def set_all_layers_readonly():
 #######################メイン########################
 # プロジェクト読み込み後に実行するpythonはこちらで実行 #
 ####################################################
-QgsProject.instance().readProject.connect(on_project_read)
-iface.mapCanvas().renderComplete.connect(on_render_complete)
-QgsApplication.taskManager().allTasksFinished.connect(on_all_tasks_finished)
+# プロジェクトが本当に読み込まれていない場合があるため、時間稼ぎが必要であり以下のメッセージ表示は削除しないこと
+# QGISの変数はすべて小文字であることに注意
+UserRole_value = QgsExpressionContextUtils.globalScope().variable('userrole')
+QMessageBox.information(None, "startup.py", "プロジェクトの読み込まれました。\n初期設定を開始します。")
+print (f"変数　UserRole：{UserRole_value}")
+if UserRole_value == 'Viewer':
+    set_all_layers_readonly()
+#QMessageBox.information(None, "startup.py", "プロジェクトの読み込みと初期化が完全に終了しました。")
+print("プロジェクトの読み込みと初期化が完全に終了しました")
 
 
 
